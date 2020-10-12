6d6d8eb4107bdf87e20384626334705558beffa1
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-10-12 23:22:06 +0100

    added a new component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
+import Quotes from "./Quotes";
 
 function App() {
-  const [quote, setQuote] = useState();
-  const [nextQuote, setNextQuote] = useState();
-  console.log(quote);
-  useEffect(() => {
-    fetch("https://fatima-node1-quote.glitch.me/quotes")
-      .then((res) => res.json())
-      .then((data) => {
-        setQuote(data);
-      });
-  }, []);
-
-  const pickRandomQuote = (arr) => {
-    return arr[Math.floor(Math.random() * arr.length)];
-  };
-
-  const chooseNextQuote = () => {
-    return setNextQuote(pickRandomQuote(quote));
-  };
-
-  if (!quote) {
-    return (
-      <div>
-        <Loader type="Bars" color="#00BFFF" height={80} width={80} />{" "}
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>Quote Generator</h1>
-        <div> {nextQuote ? nextQuote.quote : pickRandomQuote(quote).quote}</div>
-        <div>
-          {nextQuote ? nextQuote.author : pickRandomQuote(quote).author}
-        </div>
-        <button onClick={chooseNextQuote}>Next Quote</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Quotes />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Quotes.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import Loader from "react-loader-spinner";
+
+const Quotes = () => {
+  const [quote, setQuote] = useState();
+  const [nextQuote, setNextQuote] = useState();
+  console.log(quote);
+  useEffect(() => {
+    fetch("https://fatima-node1-quote.glitch.me/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data);
+      });
+  }, []);
+
+  const pickRandomQuote = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
+  const chooseNextQuote = () => {
+    return setNextQuote(pickRandomQuote(quote));
+  };
+
+  if (!quote) {
+    return (
+      <div>
+        <Loader type="Bars" color="#00BFFF" height={80} width={80} />{" "}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Quote Generator</h1>
+        <div> {nextQuote ? nextQuote.quote : pickRandomQuote(quote).quote}</div>
+        <div>
+          {nextQuote ? nextQuote.author : pickRandomQuote(quote).author}
+        </div>
+        <button onClick={chooseNextQuote}>Next Quote</button>
+      </div>
+    );
+  }
+};
+
+export default Quotes;
