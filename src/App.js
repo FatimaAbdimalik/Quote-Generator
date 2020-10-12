5554438681fabb4c81af6041a2b38294e658927a
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-10-12 22:43:22 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [quote, setQuote] = useState();
+  console.log(quote);
+  useEffect(() => {
+    fetch("https://fatima-node1-quote.glitch.me/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data);
+      });
+  }, []);
+
+  const pickRandomQuote = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
+  if (!quote) {
+    return (
+      <div>
+        <Loader type="Bars" color="#00BFFF" height={80} width={80} />{" "}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div>"{pickRandomQuote(quote).quote}"</div>
+        <div>{pickRandomQuote(quote).author}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
